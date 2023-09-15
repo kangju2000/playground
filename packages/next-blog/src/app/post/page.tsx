da506fd857d986e8619156788ee881f08ccb1e37
kang <kangju2000@naver.com>
2023-09-16 01:55:01 +0900

    refactor(next-blog): use list component in post page

--- packages/next-blog/src/app/post/page.tsx
+++ packages/next-blog/src/app/post/page.tsx
@@ -1,26 +1,17 @@
-import Link from 'next/link'
-
-import Header from '@/components/Header/Header'
+import * as styles from './styles.css'
+import { Card, List } from '@/components'
 import { getBlogPosts } from '@/utils/mdx-utils'
 
 export default async function PostsPage() {
   const posts = await getBlogPosts()
 
   return (
-    <>
-      <Header />
-      <div>
-        <ul>
-          {posts.map(({ frontmatter, type }) => (
-            <li key={frontmatter.slug}>
-              <Link href={`/${type}/${frontmatter.slug}`}>
-                <h2>{frontmatter.title}</h2>
-                <p>{frontmatter.description}</p>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <div>
+      <List
+        className={styles.list}
+        items={posts}
+        renderItem={(post) => <Card key={post.frontmatter.slug} post={post} />}
+      />
+    </div>
   )
 }
